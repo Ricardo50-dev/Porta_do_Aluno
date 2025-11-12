1999dbfb8f9d85833fbaa70b0c5d9c6eeb96a9de
Mateus Virgílio <135419333+mateusvrg@users.noreply.github.com>
2025-11-12 13:21:32 -0300

    rename admin route and add professor route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import UserRoutes from './routes/UserRoutes.js';
+import router from "./routes/AdminRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: `http://localhost:${port_app}` }));
 
 //Routes
-app.use('/users', UserRoutes)
+app.use("/users", router);
 // app.use('/training', TrainingRoutes)
 // app.use('/diet', DietRoutes)
 
