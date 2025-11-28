f8d2c104da979bd6d7c619faef30552ba8e2d08c
Tweedy <ricardocrvg50@gmail.com>
2025-11-28 19:12:03 -0300

    added to the student's schedule route

--- backend/controllers/StudentController.js
+++ backend/controllers/StudentController.js
@@ -5,6 +5,10 @@ import getUserByToken from "../helpers/get-user-by-token.js";
 import getToken from "../helpers/get-token.js";
 import Frequencia from "../models/Frequencia.js";
 import Avisos from "../models/Avisos.js";
+import Matriculas from "../models/Matriculas.js";
+import Horarios from "../models/Horarios.js";
+import { where } from "sequelize";
+import { Op } from "sequelize";
 
 export default class StudentController {
   static async minhasNotas(req, res) {
@@ -69,6 +73,37 @@ export default class StudentController {
       });
     } catch (error) {
       Logger.error(`Erro encontrar avisos. ${error}`);
+      res.status(500).json({ message: error });
+    }
+  }
+
+  static async meusHorarios(req, res) {
+    const aluno = req.aluno;
+    try {
+      const disciplinasMatriculadas = await Matriculas.findAll({
+        where: { aluno_id: aluno.ID },
+        attributes: ['disciplina_id']   // pega somente esse campo
+      });
+
+      if (disciplinasMatriculadas.length == 0) {
+        return res.status(404).json({
+          message: "Disciplinas não encontrados ou você não está matriculado em nenhuma disciplina.",
+        });
+      }
+
+      const horarios = await Horarios.findAll({
+        where: {
+          disciplina_id: {
+            [Op.in]: disciplinasMatriculadas.map(d => d.disciplina_id)
+          }
+        }
+      });
+
+      return res.status(200).json({
+        horarios,
+      });
+    } catch (error) {
+      Logger.error(`Erro encontrar horarios. ${error}`);
       res.status(500).json({ message: error });
     }
   }
